6ac9f3219d3988e962042724581860a58a8fcc29
GauravManwani <gaurav.manwani@cars24.com>
2020-08-27 15:22:47 +0530

    initial commit

---
+++ springcloudresilience/src/main/java/com/example/springcloudresilience/SpringcloudresilienceApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudresilience;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudresilienceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudresilienceApplication.class, args);
+	}
+
+}
---
+++ springcloudresilience/src/test/java/com/example/springcloudresilience/SpringcloudresilienceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudresilience;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudresilienceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
